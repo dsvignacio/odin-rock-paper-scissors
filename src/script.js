6700f2c87ae181d1047fbe38f04201638e09ecc6
Dennis Ignacio <dsvignacio@gmail.com>
2025-11-22 14:27:20 +0800

    done: rework the rock-paper-scissors project

--- src/script.js
+++ src/script.js
@@ -1,17 +1,92 @@
-// console.log("Hello")
-
 function getComputerChoice() {
-  const numRand = Math.floor(Math.random() * 3);
-  switch (numRand) {
-    case 0:
-      return "rock";
+  /*
+  Thus function gets random numbers from 0 to 2.
+
+  Since Math.random() only gives out random values between 0 and 1,
+  we want to multiply 3 to it to get a random value from 0 to 2,
+  with some additional values after the decimal point.
+
+  Next, we just want to get the whole number before the decimal point.
+
+  And finally, we add 1 to get random values from 1 to 3, instead of 0 to 2.
+  */
+  const computerChoice = Math.floor(Math.random() * 3) + 1;
+  let returnComputerChoice = null;
+  switch(computerChoice) {
     case 1:
-      return "paper";
+      returnComputerChoice = "rock";
+      break;
     case 2:
-      return "scissors";
-    default:
-      return null;
+      returnComputerChoice = "paper";
+      break;
+    case 3:
+      returnComputerChoice = "scissors";
+      break;
+  }
+  return returnComputerChoice;
+}
+
+function getHumanChoice() {
+  const humanChoice = prompt("Enter choice: rock, paper, scissors");
+  return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice == computerChoice) {
+    console.log(`It's a tie! You both chose ${humanChoice}.`);
+  } else {
+    switch(humanChoice) {
+      case "rock":
+        if(computerChoice == "paper") {
+          computerScore++;
+          console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        } 
+        else {
+          humanScore++;
+          console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        } 
+        break;
+      case "paper":
+        if(computerChoice == "scissors") {
+          computerScore++;
+          console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        } 
+        else {
+          humanScore++;
+          console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        } 
+        break;
+      case "scissors":
+        if(computerChoice == "rock") {
+          computerScore++;
+          console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        } 
+        else {
+          humanScore++;
+          console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        } 
+        break;
+    }
+  }
+
+  return [humanScore, computerScore];
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  for(let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    humanScore, computerScore = playRound(
+      humanSelection, 
+      computerSelection, 
+      humanScore, 
+      computerScore
+    );
   }
 }
 
-console.log(getComputerChoice());
+playGame()
